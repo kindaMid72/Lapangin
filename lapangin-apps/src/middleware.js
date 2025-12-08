8563470ca864b26ad228158e7ff7fe1dfde2e711
King Ahmad <kingahmadilyas03@gmail.com>
2025-12-08 18:38:08 +0800

    deploy-9

--- lapangin-apps/src/middleware.js
+++ lapangin-apps/src/middleware.js
@@ -1,17 +1,10 @@
 import { updateSession } from './utils/supabase/middleware'
 
 export async function middleware(request) {
-    // return await updateSession(request)
+    return await updateSession(request)
 }
 
 export const config = {
-    matcher: [
-        /*
-         * Cocokkan semua path request kecuali untuk:
-         * - _next/static (file statis)
-         * - _next/image (file optimasi gambar)
-         * - favicon.ico (file favicon)
-         */
-        '/((?!_next/static|_next/image|favicon.ico).*)',
-    ],
+    // Jalankan middleware di semua path
+    matcher: '/(.*)',
 }
